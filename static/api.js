a54b4f39515ab9b90ec00a47192f47fd4ff95be6
Steve Kaliski <sjkaliski@gmail.com>
2015-06-22 18:23:35 -0400

    add getResponse() and reset() methods
    add reset

--- static/api.js
+++ static/api.js
@@ -1,4 +1,5 @@
 /* PLEASE DO NOT COPY AND PASTE THIS CODE. */
+var gtcha = {}
 window.onload = function () {
   var gtchaEl = document.querySelector('.gtcha')
   var siteKey = gtchaEl.getAttribute('data-key')
@@ -8,9 +9,33 @@ window.onload = function () {
   iframe.src = '/static/gtcha.html?site_key=' + siteKey
   gtchaEl.appendChild(iframe)
 
+  var success = false
+
+  /**
+   * getResponse gets the status of the GTCHA.
+   * Returns true if the user passes, false
+   * if the user fails.
+   * @return {Boolean}
+   */
+  gtcha.getResponse = function () {
+    return success
+  }
+
+  /**
+   * In the event the GTCHA needs to be reset,
+   * the user may do so.
+   */
+  gtcha.reset = function () {
+    success = false
+    iframe.contentWindow.postMessage('reset', '*')
+  }
+
   window.addEventListener('message', function (e) {
-    if (e.data[0] == 'setHeight') {
-      iframe.style.height = e.data[1] + 'px'
+    switch (e.data[0]) {
+      case 'setHeight':
+        iframe.style.height = e.data[1] + 'px'
+      case 'setResponse':
+        success = e.data[1]
     }
   })
 }
